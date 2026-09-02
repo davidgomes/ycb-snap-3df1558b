f8419c4f4bf34a956aa3d77f83b78e4fdd090300
YourCodingBench <snapshot@yourcodingbench.com>
2026-09-02 11:27:42 +0000

    ycb snapshot: orphan @ de1202331184ccaf71154d83f03cddff05285498

---
+++ src-ui/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+const base_url = new URL(document.baseURI)
+
+export const environment = {
+  production: true,
+  apiBaseUrl: document.baseURI + 'api/',
+  apiVersion: '9', // match src/paperless/settings.py
+  appTitle: 'Paperless-ngx',
+  version: '2.16.3',
+  webSocketHost: window.location.host,
+  webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
+  webSocketBaseUrl: base_url.pathname + 'ws/',
+}
